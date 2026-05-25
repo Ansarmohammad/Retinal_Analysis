27e7ba96a2987fdaadaf211a7d891f57d8541132
Mohammad Ansar <ansarmohammad7095@gmail.com>
2026-05-25 15:16:30 +0530

    Removed autoencoder to reduce memory

--- backend/model/predict.py
+++ backend/model/predict.py
@@ -1,22 +1,18 @@
 import sys
 import json
 import torch
-import base64
-import io
 from PIL import Image
 import numpy as np
 import os
 
 from retina_classifier import RetinaClassifier
-from autoencoder import Autoencoder
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cpu"
 
 IMAGE_PATH = sys.argv[1]
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-AUTOENCODER_PATH = os.path.join(BASE_DIR, "autoencoder_retina.pth")
 CLASSIFIER_PATH = os.path.join(BASE_DIR, "retina_classifier.pth")
 
 CLASSES = [
@@ -45,35 +41,7 @@ try:
 
     img_tensor = torch.tensor(img_np).unsqueeze(0).to(device)
 
-    # AUTOENCODER
-    auto = Autoencoder().to(device)
-
-    auto.load_state_dict(
-        torch.load(AUTOENCODER_PATH, map_location=device)
-    )
-
-    auto.eval()
-
-    with torch.no_grad():
-        reconstructed_tensor = auto(img_tensor)
-        reconstructed_np = reconstructed_tensor.squeeze().cpu().numpy()
-
-    reconstructed_np = np.transpose(reconstructed_np, (1, 2, 0))
-
-    reconstructed_img = Image.fromarray(
-        (reconstructed_np * 255).astype("uint8")
-    )
-
-    # BASE64 FUNCTION
-    def to_base64(pil_img):
-        buff = io.BytesIO()
-        pil_img.save(buff, format="PNG")
-        return base64.b64encode(buff.getvalue()).decode()
-
-    original_b64 = to_base64(img)
-    reconstructed_b64 = to_base64(reconstructed_img)
-
-    # CLASSIFIER
+    # LOAD CLASSIFIER ONLY
     model = RetinaClassifier().to(device)
 
     model.load_state_dict(
@@ -83,6 +51,7 @@ try:
     model.eval()
 
     with torch.no_grad():
+
         out = model(img_tensor)
 
         prob = torch.softmax(out, dim=1)[0]
@@ -92,8 +61,6 @@ try:
         confidence = float(prob.max()) * 100
 
     result = {
-        "original": original_b64,
-        "reconstructed": reconstructed_b64,
         "stage": pred_class,
         "description": DESCRIPTIONS[pred_class],
         "confidence": confidence
